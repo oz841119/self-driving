a06a2b769bcaaa5fe20032c18b835dfdea9f7233
oz841119 <oz841119@gmail.com>
2024-01-23 17:32:28 +0800

    43:13

---
+++ js/math/graph.js
@@ -0,0 +1,47 @@
+// Graph = ( V(Nodes/Vertices), E(Links/Edges) )
+class Graph {
+    constructor(points = [], segments = []) {
+        this.points = points
+        this.segments = segments
+    }
+    addPoint(point) {
+        this.points.push(point)
+    }
+    containsPoint(point) {
+        return this.points.find((p) => p.equals(point))
+    }
+    tryAddPoint(point) {
+        if (!this.containsPoint(point)) {
+            this.addPoint(point)
+            return true
+        }
+        return false
+    }
+    addSegment(seg) {
+        this.segments.push(seg)
+    }
+    containsSegment(seg) {
+        return this.segments.find((s) => s.equals(seg))
+    }
+    tryAddSegment(seg) {
+        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg)
+            return true
+        }
+        return false
+    }
+    removeSegment(seg) {
+        this.segments.splice(this.segments.indexOf(seg), 1)
+    }
+    removePoint(point) {
+        this.points.splice(this.points.indexOf(point), 1)
+    }
+    draw(ctx) {
+        for (const seg of this.segments) {
+            seg.draw(ctx)
+        }
+        for (const point of this.points) {
+            point.draw(ctx)
+        }
+    }
+}
