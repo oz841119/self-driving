e0240a74b81bad319f50f4f70ab7d4a8d159587c
oz841119 <oz841119@gmail.com>
2024-01-25 01:03:46 +0800

    51:57

---
+++ js/graphEditor.js
@@ -0,0 +1,10 @@
+class GraphEditor {
+  constructor(canvas, graph) {
+    this.canvas = canvas
+    this.graph = graph
+    this.ctx = this.canvas.getContext('2d')
+  }
+  display() {
+    this.graph.draw(this.ctx)
+  }
+}
